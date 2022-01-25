1053c4f32e0d416a7681faf6a0008d92b2991c1d
Vishwas <VishwasDubey150@gmail.com>
2022-01-26 00:32:55 +0530

    new commands

--- spec.py
+++ spec.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pyttsx3
+#text to speech
 import datetime
 import pyaudio
 import wikipedia
@@ -11,7 +12,6 @@ import speech_recognition as sr
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice',voices[1].id)
-
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
@@ -33,25 +33,22 @@ def takeCommand():
         audio =r.listen(source)
     try:
         print("Recognizing...")
-        query=r.recognize_google(audio,language='en-in')
-        print(f"User said: {query}\n")
+        question=r.recognize_google(audio,language='en-in')
+        print(f"User said: {question}\n")
     except Exception as e:
         #print(e)
         print("Please say it again")
         return "None"
-    return query
+    return question
 if __name__ == "__main__":
     wishMe()
     while True:
-    # if 1:
-        query = takeCommand().lower() #Converting user query into lower case
-
-        # Logic for executing tasks based on query
-        if 'who' in query:  #if wikipedia found in the query then this block will be executed
+        question = takeCommand().lower()
+        if 'who' in question:
             speak('Searching Wikipedia...')
             print('searching...')
-            query = query.replace("wikipedia", "")
-            results = wikipedia.summary(query, sentences=2)
+            question = question.replace("wikipedia", "")
+            results = wikipedia.summary(question, sentences=2)
             speak("According to Wikipedia")
             print(results)
             speak(results)
@@ -59,7 +56,7 @@ if __name__ == "__main__":
             speak("Do you think detail is wrong")
             print("Do you think detail is wrong")
             takeCommand()
-            if 'yes' in query:
+            if 'yes' in question:
                 speak("Sorry for inconvenience, please search on wikipedia yourself, here you go")
                 print("Sorry for inconvenience, please search on wikipedia yourself, here you go")
                 webbrowser.open("https://www.wikipedia.org/")
@@ -67,102 +64,111 @@ if __name__ == "__main__":
                 speak("thank you for your feedback")
                 print("thank you for your feedback")
 
+        elif 'your name' in question:
+            speak("My name is Spec   and i am your voice assistant.")
 
-        elif 'your name' in query:
-            speak("My name is Spec   and i am laptop assistant of my creater Vishwas and Kaibalya")
-
-        elif 'how are you' in query:
+        elif 'how are you' in question:
             speak("I am fine, how are you sir")
 
-        elif 'open youtube' in query:
+        elif 'open youtube' in question:
             speak('opening...')
             print('opening...')
             webbrowser.open("youtube.com")
 
-        elif 'music' in query:
+        elif 'music' in question:
             speak('opening...')
             print('opening')
             webbrowser.open("https://music.youtube.com/")
 
-        elif 'google' in query:
+        elif 'google' in question:
             speak('opening')
             print('opening')
             webbrowser.open("www.google.com")
 
-        elif 'discord' in query:
+        elif 'discord' in question:
             webbrowser.open("https://discord.com/channels/@me")
             speak("Opening discord")
             print("opening discord")
 
-        elif 'Gmail' in query:
+        elif 'Gmail' in question:
             webbrowser.open("https://mail.google.com/mail/u/1/#inbox")
             speak("opening gmail")
             print("opening gmail")
 
-        elif 'joke' in query:
+        elif "i don't care" in question:
+            webbrowser.open("https://music.youtube.com/watch?v=7hDam9i-Aps&list=RDAMVM7hDam9i-Aps")
+            speak("playing")
+
+        elif "excuses" in question:
+            webbrowser.open("https://music.youtube.com/watch?v=x18b0D8sTwo&list=RDAMVMx18b0D8sTwo")
+            speak("playing")
+
+        elif 'joke' in question:
             joke=pyjokes.get_joke()
             speak(joke)
             print(joke)
 
-        elif 'LMS' in query:
+        elif 'LMS' in question:
             webbrowser.open("https://lms.bennett.edu.in/login/index.php")
             speak("Opening LMS")
             print("Opening LMS")
 
 
-        elif 'whatsapp' in query:
+        elif 'whatsapp' in question:
             speak('opening')
             print('opening')
             webbrowser.open("https://web.whatsapp.com/")
 
-        elif 'wikipedia' in query:
+        elif 'wikipedia' in question:
             speak("opening...")
             print("opening...")
             webbrowser.open('https://www.wikipedia.org/')
 
-        elif 'the time' in query:
+        elif 'the time' in question:
             strTime=datetime.datetime.now().strftime("%H:%M:%S")
             print(strTime)
             speak(f"Sir, time is {strTime}")
 
-        elif 'pycharm' in query:
+        elif 'pycharm' in question:
             codepath="D:\\PyCharm Community Edition 2021.2.2\\bin\\pycharm64.exe"
             os.startfile(codepath)
             speak("opening...")
             print("opening...")
 
-        elif 'brave' in query:
+        elif 'brave' in question:
             codepath="C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe"
             os.startfile(codepath)
             speak("opening..")
             print("opening...")
 
-        elif 'android studio' in query:
+        elif 'android studio' in question:
             codepath="D:\\android studio\\bin\\studio64.exe"
             os.startfile(codepath)
             speak("opening android studio")
             print("opening android studio")
 
-        elif 'outlook' in query:
+        elif 'outlook' in question:
             codepath="C:\\Program Files\\Microsoft Office\\root\\Office16\\OUTLOOK.EXE"
             os.startfile(codepath)
             speak("opening outlook")
             print("opening outlook")
 
-        elif "command" in query:
+        elif "command" in question:
             os.system("start cmd")
             speak("opening command prompt")
             print("opening command prompt")
 
-        elif "shut down" in query:
+        elif "shut down" in question:
             os.system("shutdown /s /t5")
 
-        elif "close" in query:
+        elif "sing" in question:
+            speak("Sorry sir, I can't sing but i am sure i few weeks i will learn to sing")
+
+
+        elif "sleep" in question:
             speak("Thanks for working with me. Bye  Bye sir ,Have a nice day ")
             sys.exit()
-
         else:
-
             print("I am still learning")
 
 
